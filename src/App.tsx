1faad9ba02a957ae5ea0c1297a938f4e998755a9
Daria Radtkina <dasharadtkina@gmail.com>
2025-12-18 20:51:29 +0200

    Fix for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import { UsersList } from "./pages/UsersList";
 import { UserDetails } from "./pages/UserDetails";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UsersList />} />
-        <Route path="/user/:userId" element={<UserDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<UsersList />} />
+      <Route path="/user/:userId" element={<UserDetails />} />
+    </Routes>
   );
 }
 
