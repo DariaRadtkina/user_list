b9beafd999e18022146044e86ae87a21175eb63d
Daria Radtkina <dasharadtkina@gmail.com>
2025-12-18 20:48:09 +0200

    Fix for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import { store } from "./store/store";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </StrictMode>,
 );
